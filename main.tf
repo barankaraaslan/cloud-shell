8c5c26e18d7124419396006287ede30319e7f5a1
Baran Karaaslan <developer.barankaraaaslan@outlook.com>
2022-02-07 16:18:44 +0300

    add terraform file to configure dockerhub repository

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    dockerhub = {
+      source = "BarnabyShearer/dockerhub"
+      version = "0.0.8"
+    }
+  }
+}
+
+provider "dockerhub" {
+}
+
+resource "dockerhub_repository" "main" {
+  name             = "cloud-shell"
+}
